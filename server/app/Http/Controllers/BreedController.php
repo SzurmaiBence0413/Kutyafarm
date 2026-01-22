c5e772b7387e48328a04230567c7b99133c0a3eb
SzurmaiBence0413 <szurmaibence56@gmail.com>
2026-01-22 09:38:32 +0100

    BreedController.php kész

--- server/app/Http/Controllers/BreedController.php
+++ server/app/Http/Controllers/BreedController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Breed;
 use App\Http\Requests\StoreBreedRequest;
 use App\Http\Requests\UpdateBreedRequest;
+use Illuminate\Database\QueryException;
 
 class BreedController extends Controller
 {
@@ -13,7 +14,21 @@ class BreedController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $rows = Breed::all();
+            $status = 200;
+            $data = [
+                'message' => 'OK',
+                'data' => $rows
+            ];
+        } catch (\Exception $e) {
+            $status = 500;
+            $data = [
+                'message' => "Server error {$e->getCode()}",
+                'data' => []
+            ];
+        }
+        return response()->json($data, $status, options: JSON_UNESCAPED_UNICODE);
     }
 
     /**
@@ -21,30 +36,97 @@ class BreedController extends Controller
      */
     public function store(StoreBreedRequest $request)
     {
-        //
+        try {
+            $row = Breed::create($request->all());
+            $data = [
+                'message' => 'OK',
+                'data' => $row
+            ];
+            return response()->json($data, 201, options: JSON_UNESCAPED_UNICODE);
+
+        } catch (QueryException $e) {
+            if ($e->getCode() == 23000 || str_contains($e->getMessage(), 'Duplicate entery')) {
+                $data = [
+                    'message' => 'Insert error: The given name alrady exist, please choose another one',
+                    'data' => [
+                        'breed' => $request->input('breed')
+                    ]
+                ];
+                return response()->json($data, 409, options: JSON_UNESCAPED_UNICODE);
+            }
+            throw $e;
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Breed $breed)
+    public function show(int $id)
     {
-        //
+        $row = Breed::find($id);
+        if ($row) {
+            $status = 200;
+            $data = [
+                'message' => 'OK',
+                'data' => $row
+            ];
+        } else {
+            $status = 404;
+            $data = [
+                'message' => "Not found id: $id",
+                'data' => null
+            ];
+        }
+        return response()->json($data, $status, options: JSON_UNESCAPED_UNICODE);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateBreedRequest $request, Breed $breed)
+    public function update(UpdateBreedRequest $request, int $id)
     {
-        //
+        $row = Breed::find($id);
+        if ($row) {
+            $row->update($request->all());
+
+            $status = 200;
+            $data = [
+                'message' => 'OK',
+                'data' => $row
+            ];
+        } else {
+            $status = 404;
+            $data = [
+                'message' => "Patch error. Not found id: $id",
+                'data' => null
+            ];
+        }
+        return response()->json($data, $status, options: JSON_UNESCAPED_UNICODE);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Breed $breed)
+    public function destroy(int $id)
     {
-        //
+        $row = Breed::find($id);
+        if ($row) {
+            $row->delete();
+
+            $status = 200;
+            $data = [
+                'message' => "OK",
+                'data' => [
+                    'id' => $id
+                ]
+            ];
+        } else {
+            $status = 404;
+            $data = [
+                'message' => "Delete error. Not found id: $id",
+                'data=' => null
+            ];
+        }
+        return response()->json($data, $status, options: JSON_UNESCAPED_UNICODE);
     }
 }
